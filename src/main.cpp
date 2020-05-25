6612bb84d006e92b0a9304b30ccf0dc5f6407614
ichinii <sven.salt@yahoo.de>
2020-05-25 22:13:00 +0000

    create submit info (use semaphores and image)

--- src/main.cpp
+++ src/main.cpp
@@ -1162,6 +1162,20 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 	std::cout << "hello vulkan" << std::endl;
 
 	while (!glfwWindowShouldClose(window)) {
+		std::uint32_t imageIndex;
+		vkAcquireNextImageKHR(device, swapchain, std::numeric_limits<std::uint64_t>::max(), semaphoreImageAvailable, VK_NULL_HANDLE, &imageIndex);
+		VkSubmitInfo submitInfo;
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.pNext = nullptr;
+    submitInfo.waitSemaphoreCount = 1;
+    submitInfo.pWaitSemaphores = &semaphoreImageAvailable;
+		VkPipelineStageFlags waitStages[] {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+    submitInfo.pWaitDstStageMask = waitStages;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &commandBuffers[imageIndex];
+    submitInfo.signalSemaphoreCount = 1;
+    submitInfo.pSignalSemaphores = &semaphoreRenderingDone;
+
 		glfwPollEvents();
 		if (glfwGetKey(window, GLFW_KEY_Q))
 			glfwSetWindowShouldClose(window, true);
